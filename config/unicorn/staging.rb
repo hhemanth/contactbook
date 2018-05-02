ddd73c773792d52116cf3f42597afdd3f86c2ce7
Hemanth Haridas <hhemanth@gmail.com>
2018-05-02 20:38:56 +0530

    changing unicorn config

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,7 +1,7 @@
 working_directory File.expand_path("../../..", __FILE__)
 worker_processes 5
-listen "/tmp/unicorn.sock"
+listen "tmp/unicorn.sock"
 timeout 30
-pid "/tmp/unicorn_contactbook.pid"
-stdout_path "/home/ec2-user/contactbook/log/unicorn.log"
-stderr_path "/home/ec2-user/contactbook/log/unicorn.log"
+pid "tmp/unicorn.pid"
+stdout_path "log/unicorn.log"
+stderr_path "log/unicorn.log"
