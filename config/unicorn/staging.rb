d36bcc7c2aaa0b7b459df3259c4b6e2a0443f274
Hemanth Haridas <hhemanth@gmail.com>
2018-05-05 16:05:40 +0530

    config unicorn.rb

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,7 +1,7 @@
 working_directory File.expand_path("../../..", __FILE__)
-worker_processes 5
-listen "tmp/sockets/unicorn.sock"
+worker_processes 4
+listen "/tmp/unicorn.sock"
 timeout 30
-pid "tmp/pids/unicorn.pid"
-stdout_path "log/unicorn.log"
-stderr_path "log/unicorn.log"
+pid "/home/ec2-user/contactbook/current/tmp/pids/unicorn.pid"
+stdout_path "/home/ec2-user/contactbook/current/log/unicorn.log"
+stderr_path "/home/ec2-user/contactbook/current/log/unicorn.log"
